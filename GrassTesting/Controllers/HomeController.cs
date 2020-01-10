08968c80f40e04eec4e335ec9eccc34876883c51
Pobochniy <pobochniy@mjr.ru>
2020-01-10 17:01:19 +0300

    first query

--- GrassTesting/Controllers/HomeController.cs
+++ GrassTesting/Controllers/HomeController.cs
@@ -5,19 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GrassTesting.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace GrassTesting.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string _baseUri = "https://ts4.travian.ru/spieler.php?uid=";
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> Cultivation()
         {
-            ViewData["Message"] = "Your application description page.";
+            await TravianLogin();
+            //var players = new int[] { 4638 };
+
+            //foreach (var player in players)
+            //{
+            //    var kk = await TravianPlayerGet(player);
+            //}
 
             return View();
         }
@@ -38,6 +49,50 @@ namespace GrassTesting.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async Task<string> TravianPlayerGet(int playerId)
+        {
+            var client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync($"{_baseUri}/spieler.php?uid={playerId}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+
+            throw new InvalidOperationException(response.StatusCode.ToString());
+
+
+        }
+
+        private async Task<string> TravianLogin()
+        {
+            var client = new HttpClient();
+
+            HttpResponseMessage responseGet = await client.GetAsync($"{_baseUri}/login.php");
+            if (!responseGet.IsSuccessStatusCode) throw new InvalidOperationException("Login GET = " + responseGet.StatusCode.ToString());
+
+            var content = await responseGet.Content.ReadAsStringAsync();
+            string htmlStart = "<input type=\"hidden\" name=\"login\" value=\"";
+            content = content.Substring(content.IndexOf(htmlStart) + htmlStart.Length);
+            string travianSysLogin = content.Substring(0, content.IndexOf("\""));
+
+            var loginModel = new TravianLoginModel
+            {
+                name = "jeer",
+                password = "moneyfor",
+                login = travianSysLogin
+            };
+
+            var dataString = JsonConvert.SerializeObject(loginModel);
+            HttpResponseMessage response = await client.PostAsync($"{_baseUri}/login.php", new StringContent(dataString, Encoding.UTF8, "application/json"));
+            if (!responseGet.IsSuccessStatusCode) throw new InvalidOperationException("Login POST = " + responseGet.StatusCode.ToString());
+            content = await responseGet.Content.ReadAsStringAsync();
+
+            return content;
         }
     }
 }
---
+++ GrassTesting/Models/TravianLoginModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GrassTesting.Models
+{
+    public class TravianLoginModel
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+        public string s1 => "Войти";
+        public string w => "1920:1080";
+        public string login { get; set; }
+        public string lowRes => "0";
+    }
+}
