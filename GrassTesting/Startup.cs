7ee4f9cfeba5400de585fd7dc1b951b08faba48b
Pobochniy <pobochniy@mjr.ru>
2020-01-10 11:20:10 +0300

    change to core 2.1

--- GrassTesting/Startup.cs
+++ GrassTesting/Startup.cs
@@ -32,7 +32,7 @@ namespace GrassTesting
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +45,6 @@ namespace GrassTesting
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
